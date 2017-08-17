a9550798bdcd23bf6b7adcb9bd5d41d2931eaa76
Perry Fustero <pjfust@gmail.com>
2017-08-17 13:23:08 -0400

    got songs to play on show pages with song-play directive

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ angular
   'ui.router',
   'ngResource'
 ])
+.config(function($sceDelegateProvider) {
+ $sceDelegateProvider.resourceUrlWhitelist([
+   // Allow same origin resource loads.
+   'self',
+   // Allow loading from our assets domain.  Notice the difference between * and **.
+   'https://open.spotify.com/embed/track/**']);
+ })
